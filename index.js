a088bd9d5c487b3a2265a2d036ecea3bfdfa4883
Peder Wiig Aalen <peder.wiig@gmail.com>
2019-03-09 17:25:16 +0100

    add readfile

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 var fs = require("fs");
 let regex = /sidebar:\s(\w*)/;
+var Promise = require("promise");
+const test1 = () => {
+  return new Promise(async (resolve, reject) => {
+    let arr = ["/uke6/", "/uke5/", "/uke4/", "/uke3/", "/uke2/", "/uke1/"];
+    let response = await resolve("asdasd");
+    return response;
+  });
+};
+const parseContent = content => {
+  let data = content.split(/\n/);
+  let sidebar = null;
+  data.some(line => {
+    let hit = line.match(regex);
+    if (hit) {
+      sidebar = line
+        .replace(/sidebar:\s|\\r|\s/g, "")
+        .split(",")
+        .map(el => {
+          return `/${el}/`;
+        });
+    }
+    return hit;
+  });
+  return sidebar;
+};
 
-const readSpecified = async () => {
-  return new Promise((resolve, reject) => {
-    fs.readFile("README.md", "utf8", (err, data) => {
+const read_file = () => {
+  let content = () => {
+    return new Promise(resolve => {
+      let content = fs.readFileSync("README.md", "utf8");
+      let response = resolve(content);
+      return response;
+    });
+  };
+  return content();
+};
+
+const reader = () => {};
+
+const readSpecified = () => {
+  return new Promise(async (resolve, reject) => {
+    await fs.readFile("README.md", "utf8", (err, data) => {
       if (err) reject(err);
       let content = data.split(/\n/);
       let sidebar = null;
@@ -24,9 +62,15 @@ const readSpecified = async () => {
     });
   });
 };
+console.log("før");
+let content = read_file()._j;
+let sidebar = parseContent(content);
+console.log("etter", sidebar);
+/*
 let main = async () => {
   let Sidebar = await readSpecified();
   console.log("specified", Sidebar);
   console.log("after calling readFile");
 };
-main();
+main();*/
+console.log("asdasd");
