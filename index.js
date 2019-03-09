6ea03746f00139bd16a426bfe9603550191d54b1
Peder Wiig Aalen <peder.wiig@gmail.com>
2019-03-09 17:28:33 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var fs = require("fs");
 let regex = /sidebar:\s(\w*)/;
 var Promise = require("promise");
-const test1 = () => {
-  return new Promise(async (resolve, reject) => {
-    let arr = ["/uke6/", "/uke5/", "/uke4/", "/uke3/", "/uke2/", "/uke1/"];
-    let response = await resolve("asdasd");
-    return response;
-  });
-};
+
 const parseContent = content => {
   let data = content.split(/\n/);
   let sidebar = null;
@@ -27,50 +21,15 @@ const parseContent = content => {
 };
 
 const read_file = () => {
-  let content = () => {
-    return new Promise(resolve => {
-      let content = fs.readFileSync("README.md", "utf8");
-      let response = resolve(content);
-      return response;
-    });
-  };
-  return content();
-};
-
-const reader = () => {};
-
-const readSpecified = () => {
-  return new Promise(async (resolve, reject) => {
-    await fs.readFile("README.md", "utf8", (err, data) => {
-      if (err) reject(err);
-      let content = data.split(/\n/);
-      let sidebar = null;
-      content.some(line => {
-        let hit = line.match(regex);
-        if (hit) {
-          sidebar = line
-            .replace(/sidebar:\s|\\r|\s/g, "")
-            .split(",")
-            .map(el => {
-              return `/${el}/`;
-            });
-        }
-        return hit;
-      });
-      if (sidebar) resolve(sidebar);
-      reject(sidebar);
-    });
+  return new Promise(resolve => {
+    let content = fs.readFileSync("README.md", "utf8");
+    let response = resolve(content);
+    return response;
   });
 };
+
 console.log("før");
 let content = read_file()._j;
 let sidebar = parseContent(content);
 console.log("etter", sidebar);
-/*
-let main = async () => {
-  let Sidebar = await readSpecified();
-  console.log("specified", Sidebar);
-  console.log("after calling readFile");
-};
-main();*/
 console.log("asdasd");
